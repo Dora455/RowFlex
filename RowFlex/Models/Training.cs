df120a6f0fa25febf73f3989f1219da73bd15f32
Dora455 <dyczek02@gmail.com>
2024-12-03 01:07:05 +0100

    DBtables

---
+++ RowFlex/Models/Training.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RowFlex.Models;
+
+public class Training
+{
+    [Key] public int Id { get; init; }
+
+    [StringLength(100)] public string? Title { get; set; }
+
+    [Required] public bool Saved { get; set; } = true;
+
+}
