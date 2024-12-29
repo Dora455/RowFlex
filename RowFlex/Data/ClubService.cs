488682b9dc2e1431b6be3782ae12b6893d9eb806
igolec <igolec@splunk.com>
2024-12-29 18:56:43 +0100

    updating club service

--- RowFlex/Data/ClubService.cs
+++ RowFlex/Data/ClubService.cs
@@ -1,14 +1,120 @@
 using RowFlex.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
+namespace RowFlex.Models;
 
 public class ClubService
 {
-    private readonly ApplicationDbContext _context;
+    private ApplicationDbContext _context;
 
     public ClubService(ApplicationDbContext context)
     {
         _context = context;
     }
+
+    public List<Club> GetClubsForCoach(string coachId)
+    {
+        return _context.Clubs
+            .Where(c => c.Coaches.Any(coach => coach.CoachId == coachId))
+            .ToList();
+    }
+    public Club GetClubById(int clubId)
+    {
+        return _context.Clubs
+            .Include(c => c.Coaches)
+            .Include(c => c.ClubMembers)
+            .FirstOrDefault(c => c.Id == clubId);
+    }
+    public User GetUserById(string userId)
+    {
+        return _context.Users.Find(userId);
+    }
+    public async Task<User?> GetUserByEmail(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+    public async Task<List<ClubMembership>> GetAllAthletesForClub(int clubId)
+    {
+        return _context.ClubMemberships
+            .Where(cm => cm.ClubId == clubId)
+            .Include(cm => cm.Athlete)
+            .ToList();
+    }
+
+    public List<Club> GetAllClubs()
+    {
+        return _context.Clubs.ToList();
+    }
+
+    public async Task AddAthleteMembership(ClubMembership membership)
+    {
+        _context.ClubMemberships.Add(membership);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddCoachToClub(ClubCoach clubCoach)
+    {
+        _context.ClubCoaches.Add(clubCoach);
+        _context.SaveChangesAsync();
+    }
+    public async Task UpdateUserClub(User user, int clubId)
+    {
+        user.ClubId = clubId; // Assign coach to the club
+        await _context.SaveChangesAsync();
+    }
+    public async Task UpdateCoachClub(User user, ClubCoach clubCoach)
+    {
+        user.ClubsAsCoach.Add(clubCoach); // Assign coach to the club
+        await _context.SaveChangesAsync();
+    }
+    public async Task AddNewClub(Club club)
+    {
+        _context.Clubs.Add(club);
+        _context.SaveChangesAsync();
+    }
+
+    public async Task AcceptAthlete(int membershipId)
+    {
+        var membership = await _context.ClubMemberships.FindAsync(membershipId);
+        if (membership != null)
+        {
+            membership.Status = MembershipStatus.Accepted;
+            membership.Athlete.ClubId = membership.ClubId;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task RejectAthlete(int membershipId)
+    {
+        var membership = await _context.ClubMemberships.FindAsync(membershipId);
+        if (membership != null)
+        {
+            membership.Status = MembershipStatus.Rejected;
+            await _context.SaveChangesAsync();
+        }
+    }
+    public List<ClubCoach> GetCoachesForClub(int clubId)
+    {
+        return _context.ClubCoaches
+            .Where(u => u.ClubId == clubId)
+            .ToList();
+    }
+
+    public ClubCoach GetCoacheClub(string userId, int clubId)
+    {
+        return _context.ClubCoaches
+            .FirstOrDefault(u => u.ClubId == clubId && u.CoachId == userId);
+    }
+    public async Task RemoveCoachFromClub(int clubCoachId)
+    {
+        var clubCoach = await _context.ClubCoaches.FindAsync(clubCoachId);
+        if (clubCoach != null)
+        {
+            _context.ClubCoaches.Remove(clubCoach);
+            await _context.SaveChangesAsync();
+        }
+    }
+
 }
-
-
