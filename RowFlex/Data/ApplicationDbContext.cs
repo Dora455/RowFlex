8c326e8d2b57939eea908e7b37ad086e57023199
igolec <igolec@splunk.com>
2025-01-09 00:44:08 +0100

    adding lazy load

--- RowFlex/Data/ApplicationDbContext.cs
+++ RowFlex/Data/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<Training> Trainings { get; set; }
     public DbSet<TrainingPlan> TrainingPlans { get; set; }
-    public DbSet<IndividualTraining> IndividualTrainings { get; set; }  
+    public DbSet<IndividualTraining> IndividualTrainings { get; set; }
     public DbSet<Presence> Presences { get; set; }
     public DbSet<Club> Clubs { get; set; }
     public DbSet<ClubMembership> ClubMemberships { get; set; }
@@ -16,4 +16,9 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Event> Events { get; set; }
     public DbSet<News> News { get; set; }
     public DbSet<WeightMeasurement> WeightMeasurements { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies();
+    }
 }
